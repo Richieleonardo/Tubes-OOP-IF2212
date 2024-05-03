81cdec48d60bf53624be159c46a8eaa75daac9d2
Salt-E <alvinfadhillah@gmail.com>
2024-05-03 16:03:12 +0700

    -Starfruit +Threepeater

--- app/src/main/java/org/example/model/plants/Starfruit.java
+++
@@ -1,7 +0,0 @@
-package org.example.model.plants;
-
-public class Starfruit extends Plant {
-    public Starfruit() {
-        super("Starfruit",150,300,20,4,5,5);
-    }
-}
---
+++ app/src/main/java/org/example/model/plants/Threepeater.java
@@ -0,0 +1,7 @@
+package org.example.model.plants;
+
+public class Threepeater extends Plant {
+    public Threepeater() {
+        super("Threepeater",250,200,25,4,-1,10);
+    }
+}
