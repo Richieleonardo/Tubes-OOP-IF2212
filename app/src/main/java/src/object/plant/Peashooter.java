69ee49fc36f91f30df7f46714ca44c4b2556d6fd
Harryts1 <118546322+Harryts1@users.noreply.github.com>
2024-05-16 14:38:46 +0700

    Update peashooter
    ada salah ngetik di peashooter

--- app/src/main/java/src/object/plant/Peashooter.java
+++ app/src/main/java/src/object/plant/Peashooter.java
@@ -5,7 +5,7 @@ import src.main.GamePanel;
 public class Peashooter extends Plant {
 
     public Peashooter(GamePanel gp){
-        super(gp, "Peashooter", 100, 100, 20, 4, -1, 10);
+        super(gp, "Peashooter", 100, 100, 25, 4, -1, 10);
 
         //name = "Peashooter";
         down1 = setup("/plant/Peashooter.png");
