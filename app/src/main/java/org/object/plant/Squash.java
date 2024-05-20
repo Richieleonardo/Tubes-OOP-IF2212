2f6857380847da2855639765f48dddfbb3530ad4
Salt-E <alvinfadhillah@gmail.com>
2024-05-20 14:37:01 +0700

    Threepeater fix

--- app/src/main/java/org/object/plant/Squash.java
+++ app/src/main/java/org/object/plant/Squash.java
@@ -6,7 +6,7 @@ import org.object.pellet.Squash_proj;
 
 public class Squash extends Plant {
 
-    public Squash(GamePanel gp){
+    public Squash(GamePanel gp) {
         super(gp, "Squash", 50, 100, 5000, 0, 1, 20, false);
         //name = "Squash";
 
@@ -17,20 +17,35 @@ public class Squash extends Plant {
     }
 
     @Override
-    public Plant clone(){
+    public Plant clone() {
         return new Squash(gp);
     }
 
-    public void update(){
+    public void update() {
         System.out.println(name + "Health : " + Health);
         Shoot();
-        int zombieIndex = gp.collisionChecker.checkEntity(this, gp.zombie);
-        if(zombieIndex != 999){
-            attackZombie(zombieIndex);
-            alive = false;
-            worldX += 48;
+
+    }
+
+    public void Shoot() {
+        //      PROJECTILE GENERATE ONLY WHEN THE OTHER IS NOT ALIVE
+            for (Entity zombie : gp.zombie) {
+                if (zombie != null) {
+                    if (zombie.worldY == this.worldY) {
+                        int distance = zombie.worldX - this.worldX;
+                        if (distance <= getRange()) {
+                            if (projectile.alive == false) {
+                                projectile.set(worldX, worldY, direction, true, this);
+
+                                //ADD PROJECTILE TO LIST
+                                gp.projectileList.add(projectile);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
 
-}
+
