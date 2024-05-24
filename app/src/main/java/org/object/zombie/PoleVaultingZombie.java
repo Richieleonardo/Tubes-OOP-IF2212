e40392ee38370d21a595a84f55dc14d41762b306
Salt-E <alvinfadhillah@gmail.com>
2024-05-24 13:33:50 +0700

    Dolphin / Pole temp

--- app/src/main/java/org/object/zombie/PoleVaultingZombie.java
+++ app/src/main/java/org/object/zombie/PoleVaultingZombie.java
@@ -6,6 +6,7 @@ import org.main.GamePanel;
 import java.util.Random;
 
 public class PoleVaultingZombie extends Zombie implements Jump{
+    static boolean hasJumped = false;
     public PoleVaultingZombie(GamePanel gp) {
         super(gp, "Pole Vaulting Zombie", 175, 1, 100, 1);
         description = "PoleVaultingZombie adalah zombie yang dapat melompati plant sebanyak 1 kali.";
@@ -36,6 +37,42 @@ public class PoleVaultingZombie extends Zombie implements Jump{
     public void jump(){
         this.worldX -=48;
         //    super.attackPlant();
+    }
+    @Override
+    public void update(){
+
+//        System.out.println("Zombie health : " + this.Health);
+        collisionOn = false;
+        gp.collisionChecker.checkTile(this);
+        //Implement collision with plant
+        int Index = gp.collisionChecker.checkEntity(this, gp.plant);
+        if(canAttack){
+            if (!hasJumped) {
+                jump();
+                attackPlant(Index);
+                hasJumped = true;
+            }
+            else{
+                attackPlant(Index);
+            }
+        }
+        else{
+            attack_counter++;
+            if(attack_counter > (60*getAttack_Speed())){
+                canAttack = true;
+                attack_counter = 0;
+            }
+            updateSlow();
+        }
+
+
+        frameCounter++;
+        if(collisionOn == false){
+            if (frameCounter > 60) {
+                worldX -= (int) (speed + 5.00);
+                frameCounter = 0;
+            }
+        }
     }
 
 }
